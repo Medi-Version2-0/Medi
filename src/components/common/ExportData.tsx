4fca40db00bd3557f27afa843dfdd875f50249cc
vivek <vivek.yadav@repozitory.com>
2024-07-15 15:33:36 +0530

    ExportData: Add send PDF, Excel & CSV through email

--- src/components/common/ExportData.tsx
+++ src/components/common/ExportData.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
-import { FaFilePdf, FaWhatsapp } from 'react-icons/fa6';
+import { FaFilePdf, FaChevronDown } from 'react-icons/fa';
 import { SiMicrosoftexcel } from 'react-icons/si';
 import { BsFiletypeCsv, BsPrinter } from 'react-icons/bs';
+import { sendEmail } from '../../helper/helper';
+import { MdOutlineAttachEmail } from 'react-icons/md';
 
 interface ExportDataProps {
   data: any[];
@@ -13,9 +15,10 @@ interface ExportDataProps {
 }
 
 const ExportData = ({ data, fields }: ExportDataProps) => {
-  const [showModal, setShowModal] = useState(false);
-  const [phoneNumbers, setPhoneNumbers] = useState('');
-  const [message, setMessage] = useState('');
+  const [emailOptionsVisible, setEmailOptionsVisible] = useState(false);
+  const [selectedFormats, setSelectedFormats] = useState<string[]>([]);
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const exportToPDF = () => {
     const doc = new jsPDF();
@@ -25,53 +28,6 @@ const ExportData = ({ data, fields }: ExportDataProps) => {
       body: data.map((item) => fields.map((field) => item[field.field])),
     });
     doc.save('exported_data.pdf');
-  };
-
-  const exportToBlob = () => {
-    const doc = new jsPDF();
-    doc.text('Exported Data', 20, 10);
-    autoTable(doc, {
-      head: [fields.map((field) => field.headerName)],
-      body: data.map((item) => fields.map((field) => item[field.field])),
-    });
-    return doc.output('blob');
-  };
-
-  const showWhatsAppModal = () => setShowModal(true);
-  const hideModal = () => setShowModal(false);
-
-  const sendWhatsApp = async () => {
-    const pdf = exportToBlob();
-    const response = await sendWhatsAppMessage(
-      phoneNumbers.split(','),
-      message,
-      pdf
-    );
-
-    if (response.success) {
-      alert('Message sent!');
-    } else {
-      alert('Failed to send message');
-    }
-    hideModal();
-  };
-
-  const sendWhatsAppMessage = async (
-    numbers: any,
-    message: string,
-    pdf: any
-  ) => {
-    const formData = new FormData();
-    formData.append('numbers', numbers);
-    formData.append('message', message);
-    formData.append('pdf', pdf, 'exported_data.pdf');
-
-    const response = await fetch('/send-whatsapp', {
-      method: 'POST',
-      body: formData,
-    });
-
-    return await response.json();
   };
 
   const exportToExcel = () => {
@@ -104,53 +60,243 @@ const ExportData = ({ data, fields }: ExportDataProps) => {
     win?.document.close();
   };
 
+  const pdfBlob = () => {
+    const doc = new jsPDF();
+    doc.text('Exported Price List ' + new Date().toLocaleString(), 20, 10);
+    autoTable(doc, {
+      head: [fields.map((field) => field.headerName)],
+      body: data.map((item) => fields.map((field) => item[field.field])),
+    });
+
+    return doc.output('blob');
+  };
+
+  const excelBlob = () => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'ExportedData');
+    const arrayBuffer = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    const blob = new Blob([arrayBuffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+    return blob;
+  };
+
+  const csvBlob = () => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const csvs = XLSX.utils.sheet_to_csv(worksheet);
+    const blob = new Blob([csvs], { type: 'text/csv;charset=utf-8;' });
+    return blob;
+  };
+
+  const handleEmail = async () => {
+    setIsLoading(true);
+
+    const attachments = [];
+
+    if (selectedFormats.includes('pdf')) {
+      attachments.push({
+        content: pdfBlob(),
+        filename: `price_list_${new Date().toLocaleString()}.pdf`,
+      });
+    }
+    if (selectedFormats.includes('excel')) {
+      attachments.push({
+        content: excelBlob(),
+        filename: `price_list_${new Date().toLocaleString()}.xlsx`,
+      });
+    }
+    if (selectedFormats.includes('csv')) {
+      attachments.push({
+        content: csvBlob(),
+        filename: `price_list_${new Date().toLocaleString()}.csv`,
+      });
+    }
+
+    await sendEmail({
+      email,
+      subject: 'Exported Price List ' + new Date().toLocaleString(),
+      message: 'Please find the attached files with the exported data.',
+      attachments,
+    });
+    setIsLoading(false);
+    toggleEmailOptions();
+  };
+
+  const toggleEmailOptions = () => {
+    setEmailOptionsVisible(!emailOptionsVisible);
+  };
+
+  const handleFormatChange = (format: string) => {
+    setSelectedFormats((prevFormats) =>
+      prevFormats.includes(format)
+        ? prevFormats.filter((f) => f !== format)
+        : [...prevFormats, format]
+    );
+  };
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    console.log(
+      '  const popupRef = useRef<HTMLDivElement>(null);',
+      popupRef.current
+    );
+    if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+      setEmailOptionsVisible(false);
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setEmailOptionsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
-      {showModal && (
-        <div className='fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-10'>
-          <div className='bg-white w-1/2 p-6 rounded-lg shadow-lg'>
-            <div className='flex justify-between items-center mb-4'>
-              <h2 className='text-xl font-semibold'>Send via WhatsApp</h2>
-              <button
-                className='text-gray-500 hover:text-gray-800'
-                onClick={hideModal}
-              >
-                &times;
-              </button>
-            </div>
-            <div className='mb-4'>
-              <textarea
-                className='w-full p-2 border border-gray-300 rounded-md'
-                placeholder='Enter phone numbers separated by commas'
-                value={phoneNumbers}
-                onChange={(e) => setPhoneNumbers(e.target.value)}
-                rows={4}
-              />
-            </div>
-            <div className='mb-4'>
-              <textarea
-                className='w-full p-2 border border-gray-300 rounded-md'
-                placeholder='Enter your message'
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                rows={4}
-              />
-            </div>
-            <div className='flex justify-end'>
-              <button
-                className='bg-green-600 text-white px-4 py-2 rounded-md shadow-md hover:bg-green-700 transition duration-300'
-                onClick={sendWhatsApp}
-              >
-                Send
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-      <div className='flex flex-row border-2 border-rose-500 min-w-[30%] max-w-full w-[330px]'>
+      <div className='flex flex-row  min-w-[30%] max-w-full w-[400px]'>
         <div className='flex flex-col gap-6'>
-          <div className='bg-slate-300 max-w-min text-nowrap'>Export Data</div>
-          <div className='flex flex-row gap-5 justify-self-end'>
+          <div className='flex flex-row gap-5'>
+            <div className='relative'>
+              <button
+                onClick={toggleEmailOptions}
+                className='flex items-center text-nowrap px-4 py-3 bg-purple-600 text-white rounded-md shadow-md hover:bg-purple-700 transition duration-300'
+              >
+                <MdOutlineAttachEmail size={25} />
+                <FaChevronDown className='ml-2' />
+              </button>
+              {emailOptionsVisible && (
+                <div
+                  ref={popupRef}
+                  className='absolute top-12  mt-1 flex flex-col items-center bg-white border rounded shadow-lg p-2 z-10 w-[250%]'
+                >
+                  <div className='flex flex-row'>
+                    <div
+                      onClick={() => handleFormatChange('pdf')}
+                      className={`block cursor-pointer px-4 py-2 hover:bg-purple-100 ${
+                        selectedFormats.includes('pdf')
+                          ? 'bg-purple-500 text-white'
+                          : ''
+                      }`}
+                    >
+                      PDF
+                    </div>
+                    <div
+                      onClick={() => handleFormatChange('excel')}
+                      className={`block cursor-pointer px-4 py-2 hover:bg-purple-100 ${
+                        selectedFormats.includes('excel')
+                          ? 'bg-purple-500 text-white'
+                          : ''
+                      }`}
+                    >
+                      Excel
+                    </div>
+                    <div
+                      onClick={() => handleFormatChange('csv')}
+                      className={`block cursor-pointer px-4 py-2 hover:bg-purple-100 ${
+                        selectedFormats.includes('csv')
+                          ? 'bg-purple-500 text-white'
+                          : ''
+                      }`}
+                    >
+                      CSV
+                    </div>
+                  </div>
+                  <div>
+                    <input
+                      type='email'
+                      placeholder='Enter recipient email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className='mt-2 w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600'
+                    />
+                    <button
+                      onClick={handleEmail}
+                      className='mt-2 w-full px-4 py-2 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 transition duration-300 flex items-center justify-center'
+                      disabled={isLoading}
+                    >
+                      {isLoading ? (
+                        <div className='w-10 h-6 flex flex-col'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 200 200'
+                          >
+                            <circle
+                              fill='orange'
+                              stroke='orange'
+                              stroke-width='15'
+                              r='15'
+                              cx='40'
+                              cy='100'
+                            >
+                              <animate
+                                attributeName='opacity'
+                                calcMode='spline'
+                                dur='2'
+                                values='1;0;1;'
+                                keySplines='.5 0 .5 1;.5 0 .5 1'
+                                repeatCount='indefinite'
+                                begin='-.4'
+                              ></animate>
+                            </circle>
+                            <circle
+                              fill='#FF156D'
+                              stroke='#FF156D'
+                              stroke-width='15'
+                              r='15'
+                              cx='100'
+                              cy='100'
+                            >
+                              <animate
+                                attributeName='opacity'
+                                calcMode='spline'
+                                dur='2'
+                                values='1;0;1;'
+                                keySplines='.5 0 .5 1;.5 0 .5 1'
+                                repeatCount='indefinite'
+                                begin='-.2'
+                              ></animate>
+                            </circle>
+                            <circle
+                              fill='#FF156D'
+                              stroke='#FF156D'
+                              stroke-width='15'
+                              r='15'
+                              cx='160'
+                              cy='100'
+                            >
+                              <animate
+                                attributeName='opacity'
+                                calcMode='spline'
+                                dur='2'
+                                values='1;0;1;'
+                                keySplines='.5 0 .5 1;.5 0 .5 1'
+                                repeatCount='indefinite'
+                                begin='0'
+                              ></animate>
+                            </circle>
+                          </svg>
+                        </div>
+                      ) : (
+                        'Send'
+                      )}
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
             <button
               onClick={exportToPDF}
               className='items-center text-nowrap px-4 py-3 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 transition duration-300'
@@ -174,12 +320,6 @@ const ExportData = ({ data, fields }: ExportDataProps) => {
               className='items-center text-nowrap px-4 py-3 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700 transition duration-300'
             >
               <BsPrinter size={25} />
-            </button>
-            <button
-              onClick={showWhatsAppModal}
-              className='items-center text-nowrap px-4 py-3 bg-green-600 text-white rounded-md shadow-md hover:bg-green-700 transition duration-300'
-            >
-              <FaWhatsapp size={25} />
             </button>
           </div>
         </div>
