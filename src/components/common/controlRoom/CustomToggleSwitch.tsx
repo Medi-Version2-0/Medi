42a866af7427664adeac7b5c435dadba19b19478
Avantikarepo <avantika.sharma@repozitory.com>
2024-07-17 10:16:21 +0530

    UI: Add DPCO discount in item settings

--- src/components/common/controlRoom/CustomToggleSwitch.tsx
+++ src/components/common/controlRoom/CustomToggleSwitch.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import '../../../index.css';
 
 const CustomToggleSwitch = ({ field, form, label, index, formik }: any) => {
-
-  const regex = /^\d+$/;
-
   const handleDiscountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (regex.test(e.target.value)) {
-      formik.setFieldValue('dpcoAct', Number(e.target.value));
+    const input = e.currentTarget.value;
+    let numericInput = input.replace(/[^0-9.]/g, '');
+    const decimalCount = (numericInput.match(/\./g) || []).length;
+    if (decimalCount > 1) {
+      numericInput = numericInput.replace(/\.(?=.*\.)/, '');
+    }
+    formik.setFieldValue('dpcoDiscount', numericInput);
+  };
+
+  const handleToggleChange = () => {
+    const newValue = !field.value;
+    form.setFieldValue(field.name, newValue);
+    if (field.name === 'dpcoAct' && !newValue) {
+      form.setFieldValue('dpcoDiscount', 0);
     }
   };
 
@@ -25,23 +34,22 @@ const CustomToggleSwitch = ({ field, form, label, index, formik }: any) => {
             {...field}
             name={field.name}
             checked={field.value}
-            onChange={() => form.setFieldValue(field.name, !field.value)}
+            onChange={handleToggleChange}
             className='opacity-0 w-0 h-0 p-2'
           />
           <span className='slider round rounded-[34px] before:rounded-[50%]'></span>
         </label>
       </div>
-      {field.name === 'dpcoAct' && field.value > 0 && (
+      {field.name === 'dpcoAct' && formik.values.dpcoAct && (
         <div className='flex flex-row justify-between items-center border-[1px] border-solid border-[#009196FF]'>
-          {' '}
           <span className='ml-[0.7rem] w-[70%] p-2 border-r-[1px] border-solid border-[#009196FF]'>
             Discount
           </span>
-          <label className=' w-[12rem] h-[1.3rem]'>
+          <label className='w-[12rem] h-[1.3rem]'>
             <input
-              name='discount'
+              name='dpcoDiscount'
               type='text'
-              value={field.value}
+              value={formik.values.dpcoDiscount}
               placeholder='Enter discount'
               className='input-class w-full h-full text-right border-none outline-none'
               onChange={handleDiscountChange}
