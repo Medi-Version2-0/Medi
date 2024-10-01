22cd19611d1706066751b0d73a342a074df255a4
PrakharRepo <prakhar.awasthi@repozitory.com>
2024-10-01 15:47:53 +0530

    MB-127: Code optimisation and add validations in party wise discount setup.

---
+++ src/constants/focusChain/partywiseDiscount.ts
@@ -0,0 +1,11 @@
+export const partywiseDiscountViewChain = [
+    'add'
+]
+
+export const createPartywiseDiscountChain = [
+    'back', 'partyId', 'custom_select_discountType', 'companyId', 'discount', 'save'
+]
+
+export const createPartywiseDiscountChainWithAllCompanies = [
+    'back', 'partyId', 'custom_select_discountType', 'discount', 'save'
+]
